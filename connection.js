e34345b2178e9630e1fc2588fac2994a8e063dc8
Nishant-Kumar-devz <nishantkumardevz@gmail.com>
2024-09-07 19:34:42 +0530

    Fix deprecated MongoDB options and update connection setup

--- connection.js
+++ connection.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose');
-function connectDB(url){
-    mongoose.connect(url).then(()=>console.log("mongoDB connected"));
+function connectDB(){
+    const url = process.env.MONGO_URI;
+    if(!url){
+        throw new Error('MONGO_URI environment variable is not defined');
+    }
+    mongoose.connect(url).then(()=>console.log("mongoDB connected")).catch(err=>console.log("MongoDB connection error:", err));
 }
 
 module.exports = connectDB;
--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
+require('dotenv').config();
 const connectDB = require('./connection');
 const route = require('./routes/url');
 
-connectDB('mongodb://127.0.0.1:27017/short-url');
+connectDB();
 app.use(express.json())
 app.use('/url',route);
 
